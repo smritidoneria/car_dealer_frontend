32ff7aa873d186204953d473a09c7bd722a6e6e3
Smriti Doneria <smritidoneria@gmail.com>
2024-05-01 19:09:26 +0530

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom for navigation
+import { Link } from 'react-router-dom'; 
 
 
 const URL="https://car-dealer-backend-2.onrender.com"
